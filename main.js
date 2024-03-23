2194a4ca4703b7c4fdcd95e6ac837a845a2a4c70
Julieta Zendri <juzendri@gmail.com>
2024-03-23 20:58:50 -0300

    cambios 2dsa

--- main.js
+++ main.js
@@ -1,53 +1,57 @@
-let nombre = prompt ("Ingrese su nombre y apellido aquí!")
+let nombre = prompt("Ingrese su nombre y apellido aquí!")
 
-let llamativa = "roja";
-let profesional = "azul";
-let basica = "blanca";
-let premium = "gris";
-
-alert ("Biendenido/a " + nombre );
-
-let pedir = prompt ("¿Ya sabe el estilo de tarjeta que desea? Ingrese SI o NO ");
-
-if ((pedir === "no") || (pedir === "No")|| (pedir === "NO") || (pedir === "nO")){
-    alert ("¡Regrese pronto!");
-} else {
-    let orden = prompt ("seleccione uno: llamativa / profesional / basica / premium");
-
-switch (orden){
-    case "llamativa": 
-    alert ("Su tarjeta ahora es " + llamativa);
-
-    break;
-
-
-
-    case "profesional": 
-    alert ("Su tarjeta ahora es " + profesional);
-
-    break;
-
-
-
-    case "basica": 
-    alert ("Su tarjeta ahora es " + basica);
-    break;
-
-
-
-    case "premium": 
-    alert ("Su tarjeta ahora es " + premium);
-    break;
-
-}
+class Estilo {
+    constructor(nombre, color) {
+        this.nombre = nombre
+        this.color = color
+    }
 }
 
-let email = prompt ("Ingrese su email aqui!")
-let telefono = prompt ("Ingrese su teléfono aqui!")
+let llamativa = new Estilo("llamativa", "roja")
+let profesional = new Estilo("profesional", "azul");
+let basica = new Estilo("basica", "blanca");
+let premium = new Estilo("premium", "gris");
 
-function despedir (){
-    alert ( nombre + ", a continuación está su tarjeta. Esperamoos que la disfrute.");
+const arrayEstilos = [llamativa, profesional, basica, premium]
+
+alert("Biendenido/a " + nombre);
+
+function crearTarjeta() {
+    let opciones = parseInt(prompt(`Pulse 1 si desea llevar una tarjeta ${arrayEstilos[0].nombre} color ${arrayEstilos[0].color}\nPulse 2 si desea llevar una tarjeta ${arrayEstilos[1].nombre} color ${arrayEstilos[1].color}Pulse 3 si desea llevar una tarjeta ${arrayEstilos[2].nombre} color ${arrayEstilos[2].color}\nPulse 4 si desea llevar una tarjeta ${arrayEstilos[3].nombre} color ${arrayEstilos[3].color}\n`))
+
+    while (opciones < 1 || opciones > 4) {
+        opciones = parseInt(prompt(`Pulse 1 si desea llevar una tarjeta ${arrayEstilos[0].nombre} color ${arrayEstilos[0].color}\nPulse 2 si desea llevar una tarjeta ${arrayEstilos[1].nombre} color ${arrayEstilos[1].color}Pulse 3 si desea llevar una tarjeta ${arrayEstilos[2].nombre} color ${arrayEstilos[2].color}\nPulse 4 si desea llevar una tarjeta ${arrayEstilos[3].nombre} color ${arrayEstilos[3].color}\n`))
+    }
+
+    let productoElegido;
+    if (opciones == 1) {
+        productoElegido = arrayEstilos[0]
+    }
+    else if (opciones == 2) {
+        productoElegido = arrayEstilos[1]
+    }
+    else if (opciones == 3) {
+        productoElegido = arrayEstilos[2]
+    }
+    else if (opciones == 4) {
+        productoElegido = arrayEstilos[3]
+    }
+
+    let confirmacion = confirm(`Usted eligió el estilo ${productoElegido.nombre} y es color ${productoElegido.color}, ¿está satisfecho/a?`)
+    if (confirmacion == true) {
+        let confirmacion2 = confirm("El producto se está generando.")
+    } else {
+        let confirmacion3 = confirm("Desea comenzar de vuelta?")
+        if (confirmacion3 == true) {
+            crearTarjeta()
+        } else {
+            alert("Ok, nos vemos pronto!")
+        }
+    }
 }
 
-despedir ()
+
+
+
+
 
